ae5d808ff942572bfc7e59fab56431917a6e9bb2
Родин Шиловский <LukasJohancen6@gmail.com>
2026-01-22 22:44:58 +0300

    feat(core): зарегистрировать SettingsService в Service Locator

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,10 +1,14 @@
 import 'package:get_it/get_it.dart';
 import '../database/app_database.dart';
+import '../services/settings_service.dart';
 
 // Глобальная переменная для доступа
 final sl = GetIt.instance;
 
 Future<void> initServices() async {
+  // В initServices()
+  final settings = await SettingsService.init();
+  sl.registerSingleton<SettingsService>(settings);
   // Регистрируем базу данных как Singleton (одна на всё приложение)
   // Используем обычный конструктор (не memory), чтобы данные сохранялись
   sl.registerLazySingleton<AppDatabase>(() => AppDatabase());
